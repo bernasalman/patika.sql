372cca4720ff30e2fd681e07ea9e8658ef186ca6
Berna Salman <56873912+bernasalman@users.noreply.github.com>
2022-08-16 10:40:43 +0300

    Create odev2.sql

---
+++ odev2.sql
@@ -0,0 +1,5 @@
+--Select * From film Where replacement_cost Between 12.99 and 16.99
+
+--Select first_name, last_name From actor Where first_name IN('Penelope', 'Nick', 'Ed')
+
+--Select * From film Where rental_rate IN(0.99, 2.99, 4.99) and replacement_cost IN(12.99, 15.99, 28.99) 
