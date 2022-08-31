c6e668b820ce8e90a4e76710e32064dfad2fa84a
Berna Salman <56873912+bernasalman@users.noreply.github.com>
2022-08-31 15:02:59 +0300

    Update odev12.sql

--- odev12.sql
+++ odev12.sql
@@ -16,13 +16,18 @@ SELECT * FROM film WHERE
 rental_rate = (SELECT MIN(rental_rate) FROM film) AND replacement_cost =(SELECT MIN(replacement_cost) FROM Film)
 );
 
-/*
---en fazla alış veriş yapan müşterileri sıralayın
-SELECT Max(alis sayısı) FROM payment WHERE coustomer_id 
-(
-SELECT Count(coustomer_id) AS 'alis sayısı' FROM payment GROUP BY(coustomer_id)
-);
-*/
+--4. sorunun farklı çözümleri;
+--en fazla alış veriş yapan müşteri idisi
+SELECT count(customer_id) AS "alışveriş sayısı", customer_id FROM payment GROUP BY customer_id ORDER BY count(customer_id) desc LIMIT 1;
 
-SELECT Count(coustomer_id) FROM payment GROUP BY(coustomer_id);
+--idsi = 1 olann custemerın alışveriş sayısını gösteren
+SELECT count(customer_id) AS "alışveriş sayısı", customer_id FROM payment WHERE customer_id = 1
+GROUP BY customer_id ORDER BY count(*) desc;
+
+--alışverişi sayısını isim, soyisim, id ile gösteren
+SELECT customer_id, first_name, last_name,(SELECT Count(*)
+	FROM payment p WHERE p.customer_id = c.customer_id GROUP BY customer_id ORDER BY Count(*) DESC) 
+FROM customer c;
+
+
 
